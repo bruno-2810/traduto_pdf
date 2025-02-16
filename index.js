25bb6db74dbc1014cb71672a02d4fa93e15a1629
brunin <bruno2810g@gmail.com>
2025-02-16 04:12:04 -0300

    .

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use("/", express.static("public"));
 app.use(fileUpload());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post("/extract-file", async (req, res) => {
     if (!req.files || !req.files.pdfFile) {
